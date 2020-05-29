c90439392fc1ce9a9fb9d59eca52e9b18663bb5d
kamilanov18 <kamilanov18@codingburgas.bg>
2020-05-30 00:27:58 +0300

    * Fix the navigation bar * Add gray_Lipa.png and white_Lupa.png for future animations * Lower font of the inspirational quotes in index.html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function sleep(ms) {
 
 async function fadeIn(previouslyGeneratedNumber) {
     tBox = $("#fadeInTextBox");
-    var texts = ['“If you can DREAM it, you can DO it.” – Walt Disney','"Success is how high you bounce when you hit bottom.” – General George Patton','“Choose a job you love, and you will never have to work a day in your life.” – Confucius','“If it scares you, it might be a good thing to try.” – Seth Godin','“Opportunities don’t happen, you create them.” – Chris Grosser','"When one door is closed, don’t you know that many more are open.” – Bob Marley"','“A person who never made a mistake never tried anything new.” – Albert Einstein'];
+    var texts = ['“If you can DREAM it, you can DO it.” – Walt Disney', '"Success is how high you bounce when you hit bottom.” – General George Patton', '“Choose a job you love, and you will never have to work a day in your life.” – Confucius', '“If it scares you, it might be a good thing to try.” – Seth Godin', '“Opportunities don’t happen, you create them.” – Chris Grosser', '"When one door is closed, don’t you know that many more are open.” – Bob Marley"', '“A person who never made a mistake never tried anything new.” – Albert Einstein'];
     var random = Math.floor(Math.random() * 7);
     if (random != previouslyGeneratedNumber) {
         tBox.html(texts[random]);
@@ -14,3 +14,11 @@ async function fadeIn(previouslyGeneratedNumber) {
     else
         fadeIn(random)
 }
+
+$(".Glass").hover(function(){
+    $(this).find("img:last").fadeToggle();
+    console.log("working");
+}, function(){
+    $(this).find("img:last").fadeToggle();
+    console.log("working");
+});
