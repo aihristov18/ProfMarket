270799da7d5ba45a7c4ff31f6cdc987175703c27
kamilanov18 <kamilanov18@codingburgas.bg>
2020-05-29 18:25:55 +0300

    Revise Home page * Attempt to fix onolad animation * Make the middle box gray

--- index.js
+++ index.js
@@ -3,14 +3,30 @@ function sleep(ms) {
 }
 
 async function fadeIn(previouslyGeneratedNumber) {
+    tBox = $("#fadeInTextBox");
     var texts = ["uga", "buga", "kef", "nekef", "staa", "nestaa", "ima", "nqma", "sluchka", "ivan"];
     var random = Math.floor(Math.random() * 10);
     if (random != previouslyGeneratedNumber) {
-        console.log(random);
-        $("#fadeInTextBox").html(texts[random]);
-        console.log(texts[random]);
-        console.log("dadada");
-        await sleep(10000);
-        fadeIn();
+        tBox.html('"'+texts[random]+'"');
+        $("#fadeInTextBox")
+            .addClass("FadeInClass")
+            console.log("add fade in")
+            $("#fadeInTextBox")
+            .one("animationend", ()=>{
+                $("#fadeInTextBox")
+                    .removeClass("FadeInClass")
+                    console.log("remove fade in")
+                    $("#fadeInTextBox")
+                    .addClass("FadeOutClass")
+                    console.log("add fade out")
+                    $("#fadeInTextBox")
+                    .one("animationend",()=>{
+                        $("#fadeInTextBox").removeClass("FadeOutClass");
+                        console.log("remove fade out")
+                    })
+                fadeIn(random);
+            })
     }
+    else
+        fadeIn(random);
 }
