b52921c10c17a7da5f64df70a0b372810f0fde35
kamilanov18 <kamilanov18@codingburgas.bg>
2020-05-29 22:54:11 +0300

    * Visual changes to index.html * Redesign the text box * Add meaningful text to alter trough the menu * change the border from red to white * Add browse offers / Register buttons

--- index.js
+++ index.js
@@ -4,29 +4,13 @@ function sleep(ms) {
 
 async function fadeIn(previouslyGeneratedNumber) {
     tBox = $("#fadeInTextBox");
-    var texts = ["uga", "buga", "kef", "nekef", "staa", "nestaa", "ima", "nqma", "sluchka", "ivan"];
-    var random = Math.floor(Math.random() * 10);
+    var texts = ['“If you can DREAM it, you can DO it.” – Walt Disney','"Success is how high you bounce when you hit bottom.” – General George Patton','“Choose a job you love, and you will never have to work a day in your life.” – Confucius','“If it scares you, it might be a good thing to try.” – Seth Godin','“Opportunities don’t happen, you create them.” – Chris Grosser','"When one door is closed, don’t you know that many more are open.” – Bob Marley"','“A person who never made a mistake never tried anything new.” – Albert Einstein'];
+    var random = Math.floor(Math.random() * 7);
     if (random != previouslyGeneratedNumber) {
-        tBox.html('"'+texts[random]+'"');
-        $("#fadeInTextBox")
-            .addClass("FadeInClass")
-            console.log("add fade in")
-            $("#fadeInTextBox")
-            .one("animationend", ()=>{
-                $("#fadeInTextBox")
-                    .removeClass("FadeInClass")
-                    console.log("remove fade in")
-                    $("#fadeInTextBox")
-                    .addClass("FadeOutClass")
-                    console.log("add fade out")
-                    $("#fadeInTextBox")
-                    .one("animationend",()=>{
-                        $("#fadeInTextBox").removeClass("FadeOutClass");
-                        console.log("remove fade out")
-                    })
-                fadeIn(random);
-            })
+        tBox.html(texts[random]);
+        await sleep(3000);
+        fadeIn(random);
     }
     else
-        fadeIn(random);
+        fadeIn(random)
 }
