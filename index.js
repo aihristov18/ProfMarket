015bcfcc4ee64d192c9739e4839d63f0262d9ff6
kamilanov18 <kamilanov18@codingburgas.bg>
2020-05-30 14:50:04 +0300

    Add smooth text transition in index.html

--- index.js
+++ index.js
@@ -2,26 +2,30 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function fadeIn(previouslyGeneratedNumber) {
+function fadeIn(previouslyGeneratedNumber) {
     tBox = $("#fadeInTextBox");
     var texts = ['“If you can DREAM it, you can DO it.” – Walt Disney', '"Success is how high you bounce when you hit bottom.” – General George Patton', '“Choose a job you love, and you will never have to work a day in your life.” – Confucius', '“If it scares you, it might be a good thing to try.” – Seth Godin', '“Opportunities don’t happen, you create them.” – Chris Grosser', '"When one door is closed, don’t you know that many more are open.” – Bob Marley"', '“A person who never made a mistake never tried anything new.” – Albert Einstein'];
     var random = Math.floor(Math.random() * 7);
-    if (random != previouslyGeneratedNumber) {
-        tBox.html(texts[random]);
-        await sleep(3000);
-        fadeIn(random);
-    }
-    else
-        fadeIn(random)
+    tBox.fadeOut('slow',()=>{
+        tBox.html(texts[random]).fadeIn('slow');
+        console.log("fadeIn completed!");
+    });
+
 }
+
 function changeGlass(direction)
 {
     if(direction)
     {
-        $("#glass").attr("src","gray_Lupa.png");
+        $("#glass").fadeOut('fast',()=>{$("#glass").attr("src","gray_Lupa.png"); $("#glass").fadeIn('fast')});
     }
     else
     {
-        $("#glass").attr("src","white_Lupa.png");
+        $("#glass").fadeOut('fast',()=>{$("#glass").attr("src","white_Lupa.png"); $("#glass").fadeIn('fast')});
     }
+}
+
+function expandSearchBar()
+{
+    $("#searchBar").css("display","auto")
 }
